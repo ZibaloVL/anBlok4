49c9f463bda9609d3d4cc4b538235a27e9a3ae90
ZibaloV <fotoroom.md@gmail.com>
2020-04-23 21:18:22 +0300

    end block4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'anBlok4';
+  isVisible = true;
+  isVisibleTagle() {
+    this.isVisible = !this.isVisible;
+  }
 }
